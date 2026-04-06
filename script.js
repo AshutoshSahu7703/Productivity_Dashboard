abf284f9b21b70bbc906869a3f038a2352ab76a4
Ashutosh Sahu <ashutoshsahuit@gmail.com>
2026-04-07 00:31:24 +0530

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let allElems=document.querySelectorAll('.elem')
 let allFullElems=document.querySelectorAll('.fullElem')
+let allFullElemsBack=document.querySelectorAll('.fullElem .class')
 allElems.forEach(function(elem){
     elem.addEventListener('click',function(){
         allFullElems[elem.id].style.display='block'
-    })
+    }) 
+})
+allFullElemsBack.forEach(function(elem){
     
 })
     
