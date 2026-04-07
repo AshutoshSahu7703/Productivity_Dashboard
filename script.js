614840f19d0d9edc09d110e374f12b5c31295a0c
Ashutosh Sahu <ashutoshsahuit@gmail.com>
2026-04-08 05:01:46 +0530

    third commit

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-let allElems=document.querySelectorAll('.elem')
+function opencard(){
+    let allElems=document.querySelectorAll('.elem')
 let allFullElems=document.querySelectorAll('.fullElem')
-let allFullElemsBack=document.querySelectorAll('.fullElem .class')
+let allFullElemsBack=document.querySelectorAll('.fullElem .back')
 allElems.forEach(function(elem){
     elem.addEventListener('click',function(){
         allFullElems[elem.id].style.display='block'
     }) 
 })
-allFullElemsBack.forEach(function(elem){
-    
+allFullElemsBack.forEach(function(elem,id){
+    elem.addEventListener('click',function(){
+        allFullElems[id].style.display = 'none'
+    }) 
 })
-    
+}  
+opencard()
