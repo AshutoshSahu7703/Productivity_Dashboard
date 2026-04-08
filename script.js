8375beb531ac29e7a199b279599cb3bb1fbb95b4
Ashutosh Sahu <ashutoshsahuit@gmail.com>
2026-04-09 00:08:40 +0530

    fourth commit

--- script.js
+++ script.js
@@ -12,5 +12,41 @@ allFullElemsBack.forEach(function(elem,id){
         allFullElems[id].style.display = 'none'
     }) 
 })
-}  
+}
 opencard()
+
+let allTask=document.querySelector('.allTask')
+
+let currentTask=[]
+
+if(localStorage.getItem('currentTask')){
+   currentTask=JSON.parse(localStorage.getItem('currentTask'))
+}
+else {
+   
+}
+function rendorTask(){
+    localStorage.setItem('currentTask',JSON.stringify(currentTask))
+    let sum = ''
+currentTask.forEach(function(e){
+    sum += `<div class="task">
+                <h4>${e.task}</h4>
+                <button>Mark as Completed</button>
+            </div>`
+})
+allTask.innerHTML=sum
+}
+rendorTask()
+
+let input = document.querySelector('.addTask form input')
+let inputDetails = document.querySelector('.addTask form textarea')
+let addBtn = document.querySelector('.addTask form button')
+let form = document.querySelector('.addTask form')
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    currentTask.push({task:input.value,description:inputDetails.value})
+    input.value=""
+    inputDetails.value=""
+    rendorTask()
+})
+
